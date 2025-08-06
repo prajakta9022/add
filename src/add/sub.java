300b68ab24aa204aea6b461624cdf745996cf06b
vrushali-138 <vrushalisangar13@gmail.com>
2025-08-06 15:11:03 +0530

    yes

--- src/add/sub.java
+++ src/add/sub.java
@@ -3,7 +3,19 @@ package add;
 public class sub {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		a=5;
+		b=3;
+		c=a+b;
+				System.out.println(c);
+				
+//				addition is 
+				
+			int d=a-b;	
+			System.out.println(d);
 
 	}
 
